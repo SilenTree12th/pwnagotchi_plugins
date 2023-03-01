673afb57f24b4ac60c36c3618d5d46f2970eafc3
SilenTree12th <45297257+SilenTree12th@users.noreply.github.com>
2023-03-01 14:00:28 +0100

    Update mycracked_pw.py

--- mycracked_pw.py
+++ mycracked_pw.py
@@ -13,18 +13,18 @@ class MyCrackedPasswords(plugins.Plugin):
     def on_loaded(self):
         logging.info("mycracked_pw loaded")
 
-    def on_handshake(self):
+    def on_handshake(self, agent, filename, access_point, client_station):
         if not os.path.exists('/home/pi/wordlists/'):
             os.makedirs('/home/pi/wordlists/')
-        # start with blank files
+        # start with blank file
         open('/home/pi/wordlists/mycracked.txt', 'w+').close()
         f=open('/root/handshakes/wpa-sec.cracked.potfile', 'r+')
-        new_lines=[]
+        all_lines=[]
         for line in f:
             pwd=line.split(':')
-            new_lines.append(pwd[-1])
+            all_lines.append(pwd[-1])
         f.close()
-        new_lines = sorted(set(new_lines))
+        new_lines = sorted(set(all_lines))
         g=open('/home/pi/wordlists/mycracked.txt','w+')
         with open('/home/pi/wordlists/mycracked.txt','w+') as g:
             for i in new_lines:
