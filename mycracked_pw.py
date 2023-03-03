ac4f55c1787710a5dd2c8486e9dc950e727b749f
SilenTree12th <45297257+SilenTree12th@users.noreply.github.com>
2023-03-03 18:05:44 +0100

    Update mycracked_pw.py

--- mycracked_pw.py
+++ mycracked_pw.py
@@ -10,7 +10,7 @@ import io
 
 class MyCrackedPasswords(plugins.Plugin):
     __author__ = '@silentree12th'
-    __version__ = '5.1.0'
+    __version__ = '5.1.1'
     __license__ = 'GPL3'
     __description__ = 'A plugin to grab all cracked passwords and creates wifi qrcodes and a wordlist which can be used for the quickdic plugin. It stores them in the home directory. Read with cat'
 
@@ -89,16 +89,16 @@ class MyCrackedPasswords(plugins.Plugin):
                     logging.info(q.read())
             except:
                 logging.error("[mycracked_pw] something went wrong generating qrcode")
-        logging.info("[mycracked_pw] qrcodes generated.")
+            logging.info("[mycracked_pw] qrcode generated.")
 
                     
-        # start with blank file
-        open('/home/pi/wordlists/mycracked.txt', 'w+').close()
+            # start with blank file
+            open('/home/pi/wordlists/mycracked.txt', 'w+').close()
         
-        #create pw list
-        new_lines = sorted(set(all_passwd))
-        with open('/home/pi/wordlists/mycracked.txt','w+') as g:
-            for i in new_lines:
-                g.write(i+"\n")
+            #create pw list
+            new_lines = sorted(set(all_passwd))
+            with open('/home/pi/wordlists/mycracked.txt','w+') as g:
+                for i in new_lines:
+                    g.write(i+"\n")
         
-        logging.info("[mycracked_pw] pw list updated")
+            logging.info("[mycracked_pw] pw list updated")
