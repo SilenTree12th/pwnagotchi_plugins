ae7ede85db1785040aebb99d8d71ad4034f01cc9
SilenTree12th <45297257+SilenTree12th@users.noreply.github.com>
2023-12-06 12:08:54 +0100

    Update better_quickdic.py

--- better_quickdic.py
+++ better_quickdic.py
@@ -18,7 +18,7 @@ Cracked handshakes stored in handshake folder as [essid].pcap.cracked
 
 class QuickDic(plugins.Plugin):
     __author__ = 'silentree12th'
-    __version__ = '1.3.1'
+    __version__ = '1.3.2'
     __license__ = 'GPL3'
     __description__ = 'Run a quick dictionary scan against captured handshakes. Optionally send found passwords as qrcode and plain text over to telegram bot.'
     __dependencies__ = {
@@ -36,7 +36,7 @@ class QuickDic(plugins.Plugin):
         self.text_to_set = ""
 
     def on_loaded(self):
-        logging.info('[quickdic] plugin loaded')
+        logging.info('[better_quickdic] plugin loaded')
 
         if 'face' not in self.options:
             self.options['face'] = '(·ω·)'
